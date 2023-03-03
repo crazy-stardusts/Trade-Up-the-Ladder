8708dc6aca3f60db7b706731c40a9361be2a3e0e
Shyam Agarwal <shyam2432061@gmail.com>
2023-03-03 19:17:10 +0530

    Added portfolio schema

--- backend/models/gameModel.js
+++ backend/models/gameModel.js
@@ -4,10 +4,22 @@ const gameSchema = new mongoose.Schema({
     name : {
         type : String,
         required : [true, "Game name is required"],
-        unique : [true, "This name is already taken"]
+        unique: [true, "This name is already taken"]
     },
     users : {
-        type : [Schema.Types.ObjectId],
+        type: [Schema.Types.ObjectId],
+        ref : 'User'
+    },
+    startingMoney: {
+        type: Number,
+        required: [true, "Starting money is required"]
+    },
+    status: {
+        type: String,
+        enum : ['not started', 'started', 'ended']
+    },
+    admin: {
+        type: Schema.Types.ObjectId,
         ref : 'User'
     }
 })
